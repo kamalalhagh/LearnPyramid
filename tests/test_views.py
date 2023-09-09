8aa338c0fb8a4eb988c14c5cb2dd550d1b41ae62
milul <kamalalhagh@gmail.com>
2023-09-09 14:09:41 +1000

    First Commit

---
+++ tests/test_views.py
@@ -0,0 +1,13 @@
+from HelloWorld.views.default import my_view
+from HelloWorld.views.notfound import notfound_view
+
+
+def test_my_view(app_request):
+    info = my_view(app_request)
+    assert app_request.response.status_int == 200
+    assert info['project'] == 'HelloWorld'
+
+def test_notfound_view(app_request):
+    info = notfound_view(app_request)
+    assert app_request.response.status_int == 404
+    assert info == {}
